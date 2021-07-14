6b899dc8885feca8249f382428b8d83dcd735acc
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-07-15 00:30:39 +0300

    2

--- js/task-06.js
+++ js/task-06.js
@@ -7,15 +7,19 @@ form.input.addEventListener('blur', checkLengthText);
 // form.input.addEventListener('change', checkInputText);
 
 function checkLengthText(event) {
-  console.log(event.currentTarget.value.length);
-  console.log(form.inputDataLength.dataset.length);
+  console.log(isNaN(event.currentTarget.value.length));
+  console.log(isNaN(form.inputDataLength.dataset.length));
 
-  const result =
-    event.currentTarget.value.length === form.inputDataLength.dataset.length
-      ? true
-      : false;
-  console.log(result);
+  if (
+    form.inputDataLength.dataset.length === event.currentTarget.value.length
+  ) {
+    console.log(false);
+  } else {
+    console.log(true);
+  }
 }
+
+// console.log(form.input);
 
 // function checkInputText(event) {
 //   console.log(event.currentTarget.value.length);
