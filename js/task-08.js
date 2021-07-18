f19dcb4da37e79d282ed3da1d7072ca0c5566264
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-07-18 21:45:57 +0300

    6

--- js/task-08.js
+++ js/task-08.js
@@ -5,7 +5,6 @@ const form = {
   destroyBtn: document.querySelector('[data-action="destroy"]'),
 };
 
-// form.input.addEventListener('click', amountInput);
 form.renderBtn.addEventListener('click', createBoxes);
 form.destroyBtn.addEventListener('click', destroyBoxes);
 
@@ -13,18 +12,14 @@ function createBoxes(amount) {
   amount = form.input.value;
   const amountBoxes = [];
   const rdmColor = () => (Math.random() * 256) >> 0;
-  const sizeValue = 30;
+  let sizeValue = 30;
 
   for (let i = 0; i < amount; i++) {
     const cloneBox = document.createElement('div');
     cloneBox.style.backgroundColor = `rgb(${rdmColor()}, ${rdmColor()}, ${rdmColor()})`;
     cloneBox.style.width = `${sizeValue}px`;
     cloneBox.style.height = `${sizeValue}px`;
-    if (i > 0) {
-      console.log(i);
-      cloneBox.style.width = `${sizeValue + 10}px`;
-      cloneBox.style.height = `${sizeValue + 10}px`;
-    }
+    sizeValue = sizeValue + 1;
     amountBoxes.push(cloneBox);
   }
   form.box.append(...amountBoxes);
